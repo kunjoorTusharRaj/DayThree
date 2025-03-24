fddf0a0ff6eadb59a7980287ce9dcaebded1a22e
tushar-19459 <19459tusharraj@gmail.com>
2025-03-24 11:20:38 +0530

    no extra line in two.java

--- two.java
+++ two.java
@@ -2,8 +2,7 @@ public class two {
 
     public static void main(String[] args) {
         try {
-            System.out.println("this is the two.java");
-            
+            System.out.println("this is the two.java"); 
         } catch (Exception e) {
             System.out.println("error "+e);
         }
