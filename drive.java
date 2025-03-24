e453ddcd69da5e38512807863852b7ea40138441
tushar-19459 <19459tusharraj@gmail.com>
2025-03-24 14:28:28 +0530

    added the input

--- drive.java
+++ drive.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+
 public class drive {
-    
+    public static void main(String[] args) {
+        System.out.println("enter age ");
+        Scanner s = new Scanner(System.in);
+        int age = s.nextInt();
+    }
 }
