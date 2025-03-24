cdc8bb9a2837cea7e2239ff23e3fb92333eefe7e
tushar-19459 <19459tusharraj@gmail.com>
2025-03-24 10:29:20 +0530

    removed the comments

--- one.java
+++ one.java
@@ -1,11 +1,8 @@
 class one{
     public static void main(String[] args) {
-        // add a excption handling methord
         try {
             System.out.println("this is class one ");
         } catch (Exception e) {
-
-        // this is the commit 
             System.out.println(e);
         }
         
