242ceab8fbf47234bd04b9dd3bbdb3e623d4d4a4
tushar-19459 <19459tusharraj@gmail.com>
2025-03-24 11:39:17 +0530

    restored

--- one.java
+++ one.java
@@ -1,11 +1,9 @@
 class one{
     public static void main(String[] args) {
-        // add a try catch block 
         try {
             System.out.println("this is class one ");
         } catch (Exception e) {
-            // TODO: handle exception
+            System.out.println(e);
         }
-        
     }
 }
