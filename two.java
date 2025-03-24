eb0ffe4df05296bc92fefc7b050410ec6c794711
tushar-19459 <19459tusharraj@gmail.com>
2025-03-24 11:01:26 +0530

    added comment in the catch block

--- two.java
+++ two.java
@@ -4,6 +4,7 @@ public class two {
         try {
             System.out.println("this is the two.java");   
         } catch (Exception e) {
+            // after pulling from add-catch
             System.out.println("error "+e);
         }
     }
