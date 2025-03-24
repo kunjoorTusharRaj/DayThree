227c57128fcb04e115311d65c09764382e49c10e
tushar-19459 <19459tusharraj@gmail.com>
2025-03-24 11:42:15 +0530

    new update

--- two.java
+++ two.java
@@ -1,11 +1,7 @@
 public class two {
     public static void main(String[] args) {
         try {
-<<<<<<< HEAD
-            System.out.println("this is the two.java");   
-=======
             System.out.println("this is the two.java"); 
->>>>>>> c2ef2c7dea6ff49cb60be87440b755bb878ef0af
         } catch (Exception e) {
             // after pulling from add-catch
             System.out.println("error "+e);
