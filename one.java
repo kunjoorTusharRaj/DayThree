a42d5867821897d14b6a9e813e7aad9c7b75e6b3
tushar-19459 <19459tusharraj@gmail.com>
2025-03-24 10:04:14 +0530

    multipal git reset soft

---
+++ one.java
@@ -0,0 +1,11 @@
+class one{
+    public static void main(String[] args) {
+        // add a excption handling methord
+        try {
+            System.out.println("this is class one ");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
+    }
+}
