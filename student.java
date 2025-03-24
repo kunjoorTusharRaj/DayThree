bc3a21fb7a372d893018c6825238eb3469fe0c81
tushar-19459 <19459tusharraj@gmail.com>
2025-03-24 15:44:42 +0530

    updated the student

---
+++ student.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class student {
+
+    public static void main(String[] args) {
+        System.out.println("enter the student details");
+        Scanner s = new Scanner(System.in);
+    }
+}
