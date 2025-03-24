43bb789b6aa12ad3e8dfec27cfbcdc196557b148
tushar-19459 <19459tusharraj@gmail.com>
2025-03-24 11:47:06 +0530

    point of reffrence

--- two.java
+++ two.java
@@ -3,7 +3,6 @@ public class two {
         try {
             System.out.println("this is the two.java"); 
         } catch (Exception e) {
-            // after pulling from add-catch
             System.out.println("error "+e);
         }
     }
