851bef4b85250afc357e4abacc5cc4447c166bc3
tushar-19459 <19459tusharraj@gmail.com>
2025-03-24 18:48:58 +0530

    added the required updates

--- car.java
+++ car.java
@@ -1,7 +1,11 @@
 public class car {
 
     public static void main(String[] args) {
-        System.out.println("this is a car ");
-        
+        try {
+            System.out.println("this is a car ");
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("erro "+e);
+        }
     }
 }
