087b5963f93e35058ba6fb36b223cefd604162fd
tushar-19459 <19459tusharraj@gmail.com>
2025-03-24 16:13:19 +0530

    student.java is updated and added the String varaibles and sysout

--- student.java
+++ student.java
@@ -5,5 +5,9 @@ public class student {
     public static void main(String[] args) {
         System.out.println("enter the student details");
         Scanner s = new Scanner(System.in);
+        String fname = s.next();
+        String name = s.next();
+        String lname = s.next();
+        System.out.println("Name :"+fname+" "+name+" "+lname);
     }
 }
