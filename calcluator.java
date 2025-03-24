2740498c2ae1e6d5ae936550fdfb6416326080d6
tushar-19459 <19459tusharraj@gmail.com>
2025-03-24 12:08:07 +0530

    added the menu

--- calcluator.java
+++ calcluator.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class calcluator {
     public static void main(String[] args) {
-        
+        while(true){
+            Scanner s = new Scanner(System.in);
+            System.out.println("=========================");
+            System.out.println("      CALCULATOR MENU    ");
+            System.out.println("=========================");
+            System.out.println("1. Addition (+)");
+            System.out.println("2. Subtraction (-)");
+            System.out.println("3. Multiplication (*)");
+            System.out.println("4. Division (/)");
+            System.out.println("5. Modulus (%)");
+            System.out.println("6. Exponentiation (^)");
+            System.out.println("7. Floor Division (//)");
+            System.out.println("8. Exit");
+            System.out.print("Enter your choice: ");
+            int op = s.nextInt();
+        }
     }
 }
