49498b648396a151fe37485abee8aa2ecec14498
tushar-19459 <19459tusharraj@gmail.com>
2025-03-24 14:51:03 +0530

    updated to add user age

---
+++ drive.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class drive {
+    public static void main(String[] args) {
+        System.out.println("enter your age");
+        Scanner s = new Scanner(System.in);
+        int age = s.nextInt();
+        
+    }
+}
