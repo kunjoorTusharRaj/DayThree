055541fb284d3b1b392bac38ce051a30f6bd93de
tushar-19459 <19459tusharraj@gmail.com>
2025-03-24 10:40:40 +0530

    updated the two.java file

--- two.java
+++ two.java
@@ -1,7 +1,11 @@
 public class two {
 
     public static void main(String[] args) {
-        // add the body
-        System.out.println("this is the two.java");
+        try {
+            System.out.println("this is the two.java");
+            
+        } catch (Exception e) {
+            System.out.println("error "+e);
+        }
     }
 }
